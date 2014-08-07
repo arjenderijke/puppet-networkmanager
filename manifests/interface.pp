af1a5d9f11e8271b70f94a3a848aaa188fc93f4e
Arjen de Rijke <arjenderijke@gmail.com>
2014-08-07 15:44:34 +0200

    Add first concat fragments

--- manifests/interface.pp
+++ manifests/interface.pp
@@ -1,6 +1,35 @@
 define networkmanager::interface (
-  $device = undef
-) {
-  $configfile = "ifcfg-${device}"
-  # $data = "NAME=\"System ${device}\"" 
+  $device = undef,
+  $nm_controlled = true,
+  $ipaddr = undef,
+  $netmask = undef,
+  $network = undef,
+  $broadcast = undef,
+  $bootproto = "dhcp",
+  $onboot = true
+)
+{
+  concat { "ifcfg-${device}":
+    ensure => present,
+  }
+
+  concat::fragment { "ifcfg-${device}_name":
+    target  => "ifcfg-${device}",
+    content => "NAME=\"System ${device}\"",
+    order   => '01',
+  }
+
+  concat::fragment { "ifcfg-${device}_device":
+    target  => "ifcfg-${device}",
+    content => "DEVICE=",
+    order   => '02',
+  }
+
+  # HWADDR=
+  # NM_CONTROLLED=
+  # IPADDR=
+  # NETMASK=
+  # NETWORK=
+  # BROADCAST=
+  # ONBOOT=
 }
