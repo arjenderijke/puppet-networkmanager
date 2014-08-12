271f9c3769b2dcef2b577c973c10f87693064862
Arjen de Rijke <arjenderijke@gmail.com>
2014-08-12 10:23:53 +0200

    finish this set of fragments and add some first logic

--- manifests/interface.pp
+++ manifests/interface.pp
@@ -1,69 +1,93 @@
 define networkmanager::interface (
   $device = undef,
   $nm_controlled = true,
+  $hwaddr = undef,
   $ipaddr = undef,
   $netmask = undef,
   $network = undef,
   $broadcast = undef,
-  $bootproto = "dhcp",
+  $bootproto = 'dhcp',
   $onboot = true
-)
-{
+) {
+  if (!defined(Class['networkmanager'])) {
+    fail('You must include the base class before defining an interface')
+  }
+
+  validate_bool(nm_controlled)
+  validate_bool(onboot)
+  validate_re($bootproto, '^(none|dhcp)$'
+
   concat { "ifcfg-${device}":
+    path   => "/etc/sysconfig/network-scripts/ifcfg-${device}",	 
     ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
   }
 
   concat::fragment { "ifcfg-${device}_name":
     target  => "ifcfg-${device}",
-    content => "NAME=\"System ${device}\"",
+    content => "NAME=\"System ${device}\"\n",
     order   => '01',
   }
 
   concat::fragment { "ifcfg-${device}_device":
     target  => "ifcfg-${device}",
-    content => "DEVICE=",
+    content => "DEVICE=${device}\n",
     order   => '02',
   }
 
   concat::fragment { "ifcfg-${device}_hwaddr":
     target  => "ifcfg-${device}",
-    content => "HWADDR=",
+    content => "HWADDR=${hwaddr}\n",
     order   => '03',
+  }
+
+  if ($nm_controlled) {
+    $controlledbynm = 'yes'
+  } else {
+    $controlledbynm = 'false'
   }
 
   concat::fragment { "ifcfg-${device}_nmcontrolled":
     target  => "ifcfg-${device}",
-    content => "NM_CONTROLLED=",
+    content => "NM_CONTROLLED=${controlledbynm}\n",
     order   => '04',
   }
 
   concat::fragment { "ifcfg-${device}_ipaddr":
     target  => "ifcfg-${device}",
-    content => "IPADDR=",
+    content => "IPADDR=${ipaddr}\n",
     order   => '05',
   }
 
   concat::fragment { "ifcfg-${device}_netmask":
     target  => "ifcfg-${device}",
-    content => "NETMASK=",
+    content => "NETMASK=${netmask}\n",
     order   => '06',
   }
 
   concat::fragment { "ifcfg-${device}_network":
     target  => "ifcfg-${device}",
-    content => "NETWORK=",
+    content => "NETWORK=${network}\n",
     order   => '07',
   }
 
   concat::fragment { "ifcfg-${device}_broadcast":
     target  => "ifcfg-${device}",
-    content => "BROADCAST=",
+    content => "BROADCAST=${broadcast}\n",
     order   => '08',
+  }
+
+  if ($onboot) {
+    $enableonboot = 'yes'
+  } else {
+    $enableonboot = 'false'
   }
 
   concat::fragment { "ifcfg-${device}_onboot":
     target  => "ifcfg-${device}",
-    content => "ONBOOT=",
+    content => "ONBOOT=${enableonboot}\n",
     order   => '09',
   }
 }
