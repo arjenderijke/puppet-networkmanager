afde00fe2b4626bcfd2b8bc6880f3d4847116cda
Arjen de Rijke <arjenderijke@gmail.com>
2015-09-22 10:06:13 +0200

    try to fix failing tests

--- spec/defines/interface_include_class_spec.rb
+++ spec/defines/interface_include_class_spec.rb
@@ -5,9 +5,10 @@ describe 'networkmanager::interface' do
   let(:title) { 'interface1' }
   let(:params) { {:device => 'eth0'} }
 
-  it do
-    expect {
-      should compile
-    }.to raise_error(Puppet::Error, /You must include the base class before defining an interface/)
+#  it do
+#    expect {
+#      should compile
+  #    }.to raise_error(Puppet::Error, /You must include the base class before defining an interface/)
+  it { should compile.and_raise_error(/You must include the base class before defining an interface/) }
   end
 end
