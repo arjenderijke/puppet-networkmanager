13b55b496088b8757446bbd072cc2e32cef699f5
Arjen de Rijke <arjenderijke@gmail.com>
2016-04-01 20:02:49 +0200

    add spec file for new define

--- manifests/ifcfg_file.pp
+++ manifests/ifcfg_file.pp
@@ -2,6 +2,17 @@
 # networkmanager ifcfg file
 #
 
-define networkmanager::ifcfg_file {
+define networkmanager::ifcfg_file (
+  $device = undef,
+)  {
+
+  concat { "ifcfg-${device}":
+    ensure => present,
+    path   => "/etc/sysconfig/network-scripts/ifcfg-${device}",
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+#    notify => Exec['nmcli_con_reload'],
+  }
 
 }
---
+++ spec/defines/ifcfg_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'networkmanager::ifcfg_file' do
+  let :facts do
+    { :concat_basedir => '/dne' } 
+  end
+
+  let(:title) { 'interface1' }
+  let(:params) { {:device => 'eth0'} }
+
+  it { should contain_concat('ifcfg-eth0') }
+
+end
